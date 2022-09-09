16e1b229b25f39c7dc949e5a2136918bb95c23b5
Ivanka Nagornyuk <IvNagornyuk@gmail.com>
2022-09-09 19:03:15 +0300

    all done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,9 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 const btn = document.querySelector('.change-color');
-const backGround = document.querySelector('body').style.backgroundColor;
+const backGround = document.querySelector('body');
+const changeText = document.querySelector('.color')
 btn.addEventListener('click', changeColor);
 function changeColor(event) {
- return event.currentTarget.backGround = getRandomHexColor;
+  const colorEl = getRandomHexColor;
+  backGround.style.backgroundColor = colorEl();
+  changeText.textContent = colorEl();
 }
+
+// done
